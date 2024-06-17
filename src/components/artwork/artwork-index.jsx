93feda0846769c283adf5dd19d25fa923462b129
Peter Eberle <petereberle@Peters-MacBook-Pro.local>
2024-06-17 09:05:36 -0700

    testing mobile menu

--- src/components/artwork/artwork-index.jsx
+++ src/components/artwork/artwork-index.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 import {GatsbyImage, getImage} from "gatsby-plugin-image"
 
@@ -11,14 +11,32 @@ import * as mediaStyles from "../styles/media.module.css"
 
 const ArtworkIndex = ({artwork}) => {
 
-	const 	[lightbox, setLightbox] = useState(''),
+	const 	[lightbox, setLightbox] = useState(false),
+			[lightboxStatus, setLightboxStatus] = useState(false),
 			isLightbox = (id) => (lightbox === id ? true : false);
+
+	useEffect( ()=>{
+
+		const windowGlobal = typeof window !== 'undefined';
+
+		if( lightbox ) { 
+
+			setLightboxStatus(true)
+
+			if(!windowGlobal) return
+
+			window.scrollTo(0,0)
+
+		} 
+		else if ( !lightbox ) { setLightboxStatus(false) }
+
+	},[lightbox])
 
 	return (
 
 		<>	
 			<div className={`${containerStyles.lightbox_toggle} ${!lightbox ? generalStyles.hide : ''}`}>
-				<MenuToggle isToggled={isLightbox} toggleMenu={setLightbox} initialState={''} visibility={lightbox} />
+				<MenuToggle isToggled={isLightbox} toggleMenu={setLightbox} initialState={false} visibility={lightbox} />
 			</div>
 
 			{ artwork.map( (data, i) => {
@@ -44,7 +62,7 @@ const ArtworkIndex = ({artwork}) => {
 		  						}
 
 		  				return (
-		  					<div key={i} className={`${containerStyles.card} ${isLightbox(id) ? containerStyles.lightbox : ''}`} onClick={ () => { setLightbox(id) }}>
+		  					<div key={i} className={`${containerStyles.card} ${isLightbox(id) ? containerStyles.lightbox : lightboxStatus ? generalStyles.no_pointer_events : ''}`} onClick={ () => { setLightbox(id) }}>
 		  						<div className={`${containerStyles.lightbox_background} ${isLightbox(id) ? containerStyles.lightbox : ''}`} />
 		  						<div className={`${containerStyles.card_wrapper} ${generalStyles.margin} ${isLightbox(id) ? containerStyles.lightbox : ''}`}>
 		  							<div className={`${containerStyles.card_landscape_inner} ${isLightbox(id) ? containerStyles.lightbox : ''}`}>
